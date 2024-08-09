f1e4345727f13a911d56dc9121999fcc602c85e3
Brunoasterio <brunoroseira@hotmail.com>
2024-08-09 15:09:22 -0300

    Create ex023.py

---
+++ ex023.py
@@ -0,0 +1,13 @@
+num = int(input("Digite um numero:"))
+
+n = str(num)
+print(30 * "*")
+print("Analisando o numero :{}".format(n))
+print(30 * "*")
+print(10 * "-=-")
+print("Unidade :{} ".format(n[3]))
+print("Dezena :{}".format(n[2]))
+print("Centena :{}".format(n[1]))
+print("Milhar :{}".format(n[0]))
+print(10 * "-=-")
+
